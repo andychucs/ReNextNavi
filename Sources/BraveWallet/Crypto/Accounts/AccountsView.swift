b72e65210978095a4ad5788682b75ffe3a76cc29
andy <andy.chu.cs@icloud.com>
2024-09-14 01:05:23 +0800

    init

---
+++ Sources/BraveWallet/Crypto/Accounts/AccountsView.swift
@@ -0,0 +1,155 @@
+/* Copyright 2021 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import UIKit
+import BraveCore
+import Combine
+import SwiftUI
+import BraveUI
+import Strings
+
+struct AccountsView: View {
+  var cryptoStore: CryptoStore
+  @ObservedObject var keyringStore: KeyringStore
+  @State private var selectedAccount: BraveWallet.AccountInfo?
+  @State private var isPresentingBackup: Bool = false
+  @State private var isPresentingAddAccount: Bool = false
+
+  private var primaryAccounts: [BraveWallet.AccountInfo] {
+    keyringStore.allAccounts.filter(\.isPrimary)
+  }
+
+  private var secondaryAccounts: [BraveWallet.AccountInfo] {
+    keyringStore.allAccounts.filter(\.isImported)
+  }
+
+  var body: some View {
+    List {
+      Section(
+        header: AccountsHeaderView(
+          keyringStore: keyringStore,
+          settingsStore: cryptoStore.settingsStore,
+          networkStore: cryptoStore.networkStore,
+          isPresentingBackup: $isPresentingBackup,
+          isPresentingAddAccount: $isPresentingAddAccount
+        )
+        .resetListHeaderStyle()
+      ) {
+      }
+      Section(
+        header: WalletListHeaderView(
+          title: Text(Strings.Wallet.primaryCryptoAccountsTitle)
+        )
+      ) {
+        ForEach(primaryAccounts) { account in
+          Button {
+            selectedAccount = account
+          } label: {
+            AddressView(address: account.address) {
+              AccountView(address: account.address, name: account.name)
+            }
+          }
+        }
+        .listRowBackground(Color(.secondaryBraveGroupedBackground))
+      }
+      Section(
+        header: WalletListHeaderView(
+          title: Text(Strings.Wallet.secondaryCryptoAccountsTitle),
+          subtitle: Text(Strings.Wallet.secondaryCryptoAccountsSubtitle)
+        )
+      ) {
+        Group {
+          let accounts = secondaryAccounts
+          if accounts.isEmpty {
+            Text(Strings.Wallet.noSecondaryAccounts)
+              .foregroundColor(Color(.secondaryBraveLabel))
+              .multilineTextAlignment(.center)
+              .frame(maxWidth: .infinity)
+              .font(.footnote.weight(.medium))
+          } else {
+            ForEach(accounts) { account in
+              Button {
+                selectedAccount = account
+              } label: {
+                AddressView(address: account.address) {
+                  AccountView(address: account.address, name: account.name)
+                }
+              }
+            }
+          }
+        }
+        .listRowBackground(Color(.secondaryBraveGroupedBackground))
+      }
+    }
+    .background(
+      NavigationLink(
+        isActive: Binding(
+          get: { selectedAccount != nil },
+          set: { if !$0 { selectedAccount = nil } }
+        ),
+        destination: {
+          if let account = selectedAccount {
+            AccountActivityView(
+              keyringStore: keyringStore,
+              activityStore: cryptoStore.accountActivityStore(
+                for: account,
+                observeAccountUpdates: false
+              ),
+              networkStore: cryptoStore.networkStore
+            )
+            .onDisappear {
+              cryptoStore.closeAccountActivityStore(for: account)
+            }
+          }
+        },
+        label: {
+          EmptyView()
+        })
+    )
+    .listStyle(InsetGroupedListStyle())
+    .listBackgroundColor(Color(UIColor.braveGroupedBackground))
+    .background(
+      Color.clear
+        .sheet(isPresented: $isPresentingBackup) {
+          NavigationView {
+            BackupWalletView(
+              password: nil,
+              keyringStore: keyringStore
+            )
+          }
+          .navigationViewStyle(StackNavigationViewStyle())
+          .environment(\.modalPresentationMode, $isPresentingBackup)
+          .accentColor(Color(.braveBlurpleTint))
+        }
+    )
+    .background(
+      Color.clear
+        .sheet(isPresented: $isPresentingAddAccount) {
+          NavigationView {
+            AddAccountView(
+              keyringStore: keyringStore,
+              networkStore: cryptoStore.networkStore
+            )
+          }
+          .navigationViewStyle(StackNavigationViewStyle())
+        }
+    )
+  }
+}
+
+#if DEBUG
+struct AccountsViewController_Previews: PreviewProvider {
+  static var previews: some View {
+    Group {
+      AccountsView(
+        cryptoStore: .previewStore,
+        keyringStore: .previewStoreWithWalletCreated
+      )
+    }
+    .previewLayout(.sizeThatFits)
+    .previewColorSchemes()
+  }
+}
+#endif
---
+++ Sources/BraveWallet/NetworkIcon.swift
@@ -0,0 +1,60 @@
+// Copyright 2022 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import BraveCore
+import SwiftUI
+import BraveUI
+
+struct NetworkIcon: View {
+  
+  var network: BraveWallet.NetworkInfo
+
+  @ScaledMetric var length: CGFloat = 30
+  
+  var body: some View {
+    Group {
+      if let (iconName, grayscale) = networkImageInfo {
+        Image(iconName, bundle: .module)
+          .resizable()
+          .aspectRatio(contentMode: .fit)
+          .saturation(grayscale ? 0 : 1)
+      } else if let urlString = network.iconUrls.first,
+                let url = URL(string: urlString) {
+        WebImageReader(url: url) { image in
+          if let image = image {
+            Image(uiImage: image)
+              .aspectRatio(contentMode: .fit)
+              .clipShape(Circle())
+          } else {
+            networkIconMonogram
+          }
+        }
+      } else {
+        networkIconMonogram
+      }
+    }
+    .aspectRatio(1, contentMode: .fit)
+    .frame(width: length, height: length)
+  }
+  
+  private var networkIconMonogram: some View {
+    Blockie(address: network.chainName, shape: .circle)
+      .overlay(
+        Text(network.chainName.first?.uppercased() ?? "")
+          .font(.system(size: length / 2, weight: .bold, design: .rounded))
+          .foregroundColor(.white)
+          .shadow(color: .black.opacity(0.3), radius: 2, x: 0, y: 1)
+      )
+  }
+  
+  private typealias NetworkImageInfo = (iconName: String, grayscale: Bool)
+  private var networkImageInfo: NetworkImageInfo? {
+    let isGrayscale = WalletConstants.supportedTestNetworkChainIds.contains(network.chainId)
+    if let imageName = network.networkLogoName {
+      return (imageName, isGrayscale)
+    }
+    return nil
+  }
+}
